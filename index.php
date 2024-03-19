35ddf88dac6147ee88fc9f17ddfb6f9328299896
Casmir Onyeka <casmir293@gmail.com>
2024-03-19 15:57:30 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PHP Session</title>
+  </head>
+  <body>
+    <?php
+    $name = "Casmir";
+    echo "My first PHP script by $name!";
+    ?>
+  </body>
+</html>
