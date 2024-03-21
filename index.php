2ad9a3593f1944b6502839365c42f0052a48f4eb
Casmir Onyeka <casmir293@gmail.com>
2024-03-21 21:33:31 +0100

    commit

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-  <form action="index.php" method="get">
+  <form action="index.php" method="post">
     <label for="name">Name:</label> <br>
     <input type="text" name="username" placeholder="username">
     <br><br>
@@ -21,6 +21,6 @@
 </html>
 
 <?php
-echo "{$_GET['username']} <br>";
-echo "{$_GET['password']} <br>";
+echo "{$_POST['username']} <br>";
+echo "{$_POST['password']} <br>";
 ?>
